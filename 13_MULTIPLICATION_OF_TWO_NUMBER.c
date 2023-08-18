8e9281a0c1d1393b62e740c4c26cc3b82fd189ad
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-08-18 18:37:25 +0530

    Day 13 commit

---
+++ 13_MULTIPLICATION_OF_TWO_NUMBER.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("value of a is \n");
+scanf("%d",&a); 
+printf("value of b is \n ");
+scanf("%d",&b);
+printf("mul of a and b is %d",a*b);
+return 0;
+}
