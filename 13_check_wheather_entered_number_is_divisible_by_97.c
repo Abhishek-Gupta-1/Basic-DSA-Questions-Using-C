6a3a70fd87f20ee42b565938180d9328d227647c
Abhishek Gupta <abhisheknew2023@gmail.com>
2023-08-18 18:38:51 +0530

    Day 13 commit

---
+++ 13_check_wheather_entered_number_is_divisible_by_97.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+     int a;
+     printf("enter number :");
+     scanf("%d",&a);
+     if (a%97==0)
+     printf("your number is divisible by 97");
+     else
+     {
+         printf("you number is not divisible by 97");
+     }
+     
+     
+     return 0;
+}
